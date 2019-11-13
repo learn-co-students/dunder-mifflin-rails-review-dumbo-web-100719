1963e51984a49719d6b05e5fe5dbb98785f85c86
Javone <javosmith12@gmail.com>
2019-11-13 13:30:48 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,37 @@
 class EmployeesController < ApplicationController
+    def index
+        @employees = Employee.all
+        @dogs = Dog.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(strong_params)
+        @employee.save
+        redirect_to @employee
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(strong_params)
+        redirect_to @employee
+    end
+    
+    
+    private
+
+    def strong_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office)
+    end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+    belongs_to :dog
+
+    validates :alias, presence: true
+    validates :title, presence: true
 end
